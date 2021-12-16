2f0e54df8607dfa412195058070e6ac7844fdbba
wiktorkoropczuk <wiktor.koropczuk@student.put.poznan.pl>
2021-12-16 09:47:48 +0100

    Dodane kilka regul i wykrywanie wyniku

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
+import org.kie.api.builder.Results;
 import org.kie.api.definition.type.FactType;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -14,19 +15,16 @@ import org.kie.api.runtime.rule.QueryResultsRow;
 
 public class DroolsTest {
     public static final void main(String[] args) {
-        try {
-            
-	        KieServices ks = KieServices.Factory.get();
+        try{
+        	KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
         	KieBase kbase = kSession.getKieBase();
-
-            for (;;)
-            {
+        	QueryResults results;
+            do{
             	kSession.fireAllRules();
-            	QueryResults results = kSession.getQueryResults("Questions without answers");
-            	for (QueryResultsRow result : results)
-            	{
+            	results = kSession.getQueryResults("Questions without answers");
+            	for (QueryResultsRow result : results){
             		Object obj = result.get("question");
             		FactType questionType = kbase.getFactType("com.sample", "Question");
             		String question = (String)questionType.get(obj, "question");
@@ -42,9 +40,15 @@ public class DroolsTest {
             	    FactHandle handle = kSession.getFactHandle(obj);
             	    kSession.update(handle, obj);
             	}
-            	
-            }
-            
+            	results = kSession.getQueryResults("Get answer");
+           }
+           while (results.size() == 0);
+           for (QueryResultsRow result : results){
+        		Object obj = result.get("answer");
+        		FactType questionType = kbase.getFactType("com.sample", "FinalAnswer");
+        		String finalAnswer = (String)questionType.get(obj, "answer");
+        		JOptionPane.showMessageDialog(null, finalAnswer);
+           }
         } catch (Throwable t) {
             t.printStackTrace();
         }
