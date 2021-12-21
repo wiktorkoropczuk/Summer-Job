52a31d02eb63518ea5915b8ec07ec377de26f1ed
Agnieszka Klimek <69504909+agak33@users.noreply.github.com>
2021-12-21 18:24:46 +0100

    Add files via upload

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -4,12 +4,15 @@ import javax.swing.JOptionPane;
 
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
+import org.kie.api.builder.Results;
 import org.kie.api.definition.type.FactType;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.rule.FactHandle;
 import org.kie.api.runtime.rule.QueryResults;
 import org.kie.api.runtime.rule.QueryResultsRow;
+
+import org.kie.api.logger.*;
 
 
 public class DroolsTest {
@@ -18,6 +21,7 @@ public class DroolsTest {
         	KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
+        	KieRuntimeLogger kLogger = ks.getLoggers().newFileLogger(kSession, "test");
         	KieBase kbase = kSession.getKieBase();
         	QueryResults results;
             do{
@@ -35,6 +39,10 @@ public class DroolsTest {
             	        case JOptionPane.NO_OPTION:
             	        	questionType.set(obj, "answer", "No");
             	        	break;
+            	        case JOptionPane.CANCEL_OPTION:
+            	        	System.exit(0);
+            	        case JOptionPane.CLOSED_OPTION:
+            	        	System.exit(0);
             	    }
             	    FactHandle handle = kSession.getFactHandle(obj);
             	    kSession.update(handle, obj);
@@ -42,11 +50,12 @@ public class DroolsTest {
             	results = kSession.getQueryResults("Get answer");
            }
            while (results.size() == 0);
+           kLogger.close();
            for (QueryResultsRow result : results){
         		Object obj = result.get("answer");
         		FactType questionType = kbase.getFactType("com.sample", "FinalAnswer");
         		String finalAnswer = (String)questionType.get(obj, "answer");
-        		JOptionPane.showMessageDialog(null, finalAnswer);
+        		JOptionPane.showMessageDialog(null, "This summer you should be:\n" + finalAnswer);
            }
         } catch (Throwable t) {
             t.printStackTrace();
